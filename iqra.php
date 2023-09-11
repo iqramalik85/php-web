2600bbfe7494ade52b2abd833f090bc9ab6067de
iqramalik85 <malikayra85@gmail.com>
2023-09-11 16:04:47 +0500

    Initial commit

---
+++ iqra.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Web App</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Web App</h1>
+    <?php
+    // PHP code to display a message
+    $message = "Hello, world!";
+    echo "<p>$message</p>";
+    ?>
+</body>
+</html>
+
